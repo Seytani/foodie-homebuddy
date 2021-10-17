443323cd0bf977e1cf35692455a594ed7e421695
Jose Garza <josel.garza@outlook.com>
2021-10-17 18:10:20 -0500

    Update src/store/store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth-slice";
-import recipesReducer from "./recipes-slice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth-slice';
+import recipesReducer from './recipes-slice';
 
 export const store = configureStore({
 	reducer: {
